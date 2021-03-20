34a3510b840e565625c847646cb36cdc56f519ac
brunopardini <bpardini@k2partnering.com>
2021-03-20 07:20:07 -0300

    Added multi party consensus and some tests

--- Course6-Exercises/Exercise A/contracts/ExerciseC6A.sol
+++ Course6-Exercises/Exercise A/contracts/ExerciseC6A.sol
@@ -6,7 +6,7 @@ contract ExerciseC6A {
     /*                                       DATA VARIABLES                                     */
     /********************************************************************************************/
 
-
+    uint constant M = 2;
     struct UserProfile {
         bool isRegistered;
         bool isAdmin;
@@ -15,7 +15,9 @@ contract ExerciseC6A {
     address private contractOwner;                  // Account used to deploy contract
     mapping(address => UserProfile) userProfiles;   // Mapping for storing user profiles
 
+    bool private operational = true;
 
+    address[] multiCalls = new address[](0);
 
     /********************************************************************************************/
     /*                                       EVENT DEFINITIONS                                  */
@@ -51,6 +53,17 @@ contract ExerciseC6A {
         _;
     }
 
+    /**
+    * @dev Modifier that requires the "operational" boolean variable to be "true"
+    *      This is used on all state changing functions to pause the contract in 
+    *      the event there is an issue that needs to be fixed
+    */
+    modifier requireIsOperational() 
+    {
+        require(operational, "Contract is currently not operational");
+        _;  // All modifiers require an "_" which indicates where the function body will be added
+    }
+
     /********************************************************************************************/
     /*                                       UTILITY FUNCTIONS                                  */
     /********************************************************************************************/
@@ -72,6 +85,19 @@ contract ExerciseC6A {
         return userProfiles[account].isRegistered;
     }
 
+    /**
+    * @dev Get operating status of contract
+    *
+    * @return A bool that is the current operating status
+    */      
+    function isOperational() 
+                            public 
+                            view 
+                            returns(bool) 
+    {
+        return operational;
+    }
+
     /********************************************************************************************/
     /*                                     SMART CONTRACT FUNCTIONS                             */
     /********************************************************************************************/
@@ -82,6 +108,7 @@ contract ExerciseC6A {
                                     bool isAdmin
                                 )
                                 external
+                                requireIsOperational
                                 requireContractOwner
     {
         require(!userProfiles[account].isRegistered, "User is already registered.");
@@ -90,6 +117,39 @@ contract ExerciseC6A {
                                                 isRegistered: true,
                                                 isAdmin: isAdmin
                                             });
+    }
+
+    /**
+    * @dev Sets contract operations on/off
+    *
+    * When operational mode is disabled, all write transactions except for this one will fail
+    */    
+    function setOperatingStatus
+                            (
+                                bool mode
+                            ) 
+                            external 
+    {
+        require(mode != operational, "New mode must be different from existing mode");
+        require(userProfiles[msg.sender].isAdmin, "Caller is not an admin");
+
+        //TODO Store the names in a mapping for an optimization
+        bool isDuplicate = false;
+        for(uint c = 0; c < multiCalls.length; c++) { //Pay attetntion for it - Should it be here? It could hit the block gas limit, preventing the contract from running any further
+            if (multiCalls[c] == msg.sender) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        // -> Avoid using arrays in scenarios which it grows very long, 10 or 20 items.
+            // -> The best solution for that is having an external client perform the task of repeatedly calling the function with each entity that is there
+        require(!isDuplicate, "Caller has already called this function.");
+
+        multiCalls.push(msg.sender);
+        if (multiCalls.length >= M) {
+            operational = mode;      
+            multiCalls = new address[](0); //important - if you forget that, every subsequent call would have the length condition pass
+        }
     }
 }
 
